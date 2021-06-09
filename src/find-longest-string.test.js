66467f604286ba88af2b5e83c5f5d1997ffd6191
Shifahu Lawal <shifahu@hotmail.co.uk>
2021-06-09 15:59:11 +0100

    Files restored

--- src/find-longest-string.test.js
+++ src/find-longest-string.test.js
@@ -8,4 +8,6 @@ test("findLongestString finds the longest string in an array", () => {
 
 test("findLongestString returns the earlier string in cases of joint longest strings", () => {
   expect(findLongestString(["brave", "dance"])).toBe("brave");
+  expect(findLongestString(["one", "two", "three"])).toBe("three");
+  expect(findLongestString(["happy", "sad", "tiredness"])).toBe("tiredness");
 });
